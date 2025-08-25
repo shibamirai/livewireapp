35daf4ac5de5f339bbf56d7703326333000c86bf
shibamirai <t_shiba@miraino-katachi.co.jp>
2025-08-25 10:19:39 +0900

    リスト 8-18

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class HelloController extends Controller
 {
@@ -28,9 +29,16 @@ class HelloController extends Controller
     {
         $email = $request->email;
         $password = $request->password;
-        if (Auth::attempt(['email' => $email, 'password' => $password], false)) {
-            return redirect('/hello');
+        $user = User::where('email', $email)->first();
+        if ($user) {
+            if (Hash::check($password, $user->password)) {
+                Auth::login($user);
+                return redirect('/hello');
+            } else {
+                return redirect('/hello/login');
+            }
+        } else {
+            return redirect('/hello/login');
         }
-        return redirect('/hello/login');
     }
 }
